e410813652496b1b7c78d32d8fe110561e4fb161
Sthofy <suhajdakristof@gmail.com>
2022-09-21 18:58:02 +0200

    Add functionality to LogIn button
    Added functionality to LogIn button.
    Now we can authenticate the user and save logged user data.

--- Program/LoginPage.py
+++ Program/LoginPage.py
@@ -1,6 +1,20 @@
 from kivy.uix.screenmanager import Screen
 from kivy.lang import Builder
+import DB_Manager
 
 
 class LoginWidget(Screen):
     Builder.load_file('KV-files/loginpage.kv')
+
+    logged_in_user = ''
+
+    def get_logged_in_user(self):
+        return self.logged_in_user
+
+    def login(self):
+        username = self.ids.login_username.text
+        password = self.ids.login_password.text
+        logged_in_user = DB_Manager.login_user(username, password)
+
+        if (logged_in_user.uid != None):
+            self.parent.current = 'main'
