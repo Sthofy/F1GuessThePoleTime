facac3a9f158209d5b0c5acc8ed480e62f83e65a
Sthofy <suhajdakristof@gmail.com>
2022-09-21 18:59:54 +0200

    Add new method
    Add a login_user method for authenticate the user.

--- Program/DB_Manager.py
+++ Program/DB_Manager.py
@@ -1,4 +1,5 @@
 import sqlite3
+import Models.LoggedInUserModel as LoggedInUserModel
 
 conn = None
 curs = None
@@ -21,10 +22,32 @@ def register_user(username, email, password, phone):
 
     if ((len(username) > 0) & (len(email) > 0) & (len(password) > 0) & (len(phone) > 0)):
         try:
-            curs.execute("INSERT INTO User_Credential (username, email, password, phone) VALUES (?, ?, ?, ?)", (username, email, password, phone))
+            curs.execute("INSERT INTO User_Credential (username, email, password, phone) VALUES (?, ?, ?, ?)",
+                         (username, email, password, phone))
             conn.commit()
             output = True
         except Exception as e:
             print(e)
 
     return output
+
+
+def login_user(username, password):
+    try:
+        line = []
+        connect()
+        curs.execute(f'''SELECT * FROM User_Credential WHERE username="{username}" AND password="{password}"''')
+        datas = curs.fetchall()
+        for data in datas:
+            line = data
+
+        LoggedInUserModel.uid = line[0]
+        LoggedInUserModel.username = line[1]
+        LoggedInUserModel.email = line[2]
+        LoggedInUserModel.password = line[3]
+        LoggedInUserModel.phone = line[4]
+
+    except Exception as e:
+        print(e)
+
+    return LoggedInUserModel
