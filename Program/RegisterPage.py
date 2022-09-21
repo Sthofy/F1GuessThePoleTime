a7c1db6b7c637976b05f0153768516ba71b43b44
Sthofy <suhajdakristof@gmail.com>
2022-09-21 16:26:06 +0200

    UI update
    Added more visual effects.

--- Program/RegisterPage.py
+++ Program/RegisterPage.py
@@ -1,6 +1,18 @@
 from kivy.uix.screenmanager import Screen
 from kivy.lang import Builder
+import DB_Manager
+
+DB_Manager.connect()
 
 
 class RegisterWidget(Screen):
     Builder.load_file('KV-files/registerpage.kv')
+
+    def get_data(self):
+        username = self.ids.register_username.text
+        email = self.ids.register_email.text
+        password = self.ids.register_password.text
+        phone = self.ids.register_phone.text
+        isSuccess = DB_Manager.register_user(username, email, password, phone)
+        if (isSuccess):
+            self.parent.current = 'login'
--- Program/main.py
+++ Program/main.py
@@ -1,16 +1,13 @@
-import os
-
-cwd = os.getcwd()
-os.environ['KIVY_HOME'] = cwd + '/conf'
-
 from kivymd.app import MDApp
 from kivy.uix.screenmanager import ScreenManager
+from kivy.core.window import Window
 from kivy.lang import Builder
 from LoginPage import LoginWidget
 from MainPage import MainWidget
 from RegisterPage import RegisterWidget
 
 sm = ScreenManager()
+Window.size = (600, 800)
 
 
 class Main(MDApp):
@@ -25,11 +22,12 @@ class Main(MDApp):
     def build(self):
         self.root = Builder.load_file('KV-files/root.kv')
 
-        screens = [LoginWidget(name="login"), MainWidget(name='main'),RegisterWidget(name='register')]
+        screens = [LoginWidget(name="login"), MainWidget(name='main'), RegisterWidget(name='register')]
 
         for screen in screens:
             sm.add_widget(screen)
-        sm.current = "login"
+
+        self.change_screen('login')
         return sm
 
 
