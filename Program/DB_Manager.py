fb7de88f0769a33d402d6e4f001524283f979943
Sthofy <suhajdakristof@gmail.com>
2022-09-21 16:47:28 +0200

    Add Some method
    Add connect method for connect to the database.
    Add close method for close the transaction.
    Add register_user method for insert user to User_Credential.

--- Program/DB_Manager.py
+++ Program/DB_Manager.py
@@ -1,7 +1,30 @@
 import sqlite3
 
-# Létrehozás(ha nem létezik) és kapcsolódás
-conn = sqlite3.connect("Database/F1Guess.db")
-curs = conn.cursor()
+conn = None
+curs = None
 
-conn.close()
+
+def connect():
+    global conn, curs
+    conn = sqlite3.connect("Database/F1Guess.db")
+    curs = conn.cursor()
+
+
+def close():
+    if conn and curs:
+        curs.close()
+        conn.close()
+
+
+def register_user(username, email, password, phone):
+    output = False
+
+    if ((len(username) > 0) & (len(email) > 0) & (len(password) > 0) & (len(phone) > 0)):
+        try:
+            curs.execute("INSERT INTO User_Credential (username, email, password, phone) VALUES (?, ?, ?, ?)", (username, email, password, phone))
+            conn.commit()
+            output = True
+        except Exception as e:
+            print(e)
+
+    return output
