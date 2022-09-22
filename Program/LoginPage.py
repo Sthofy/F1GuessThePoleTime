e0479af7244e64dc667a8ee6ad60db17b3d18e20
Sthofy <suhajdakristof@gmail.com>
2022-09-22 13:51:30 +0200

    Add Show/Hide Password Functionality

--- Program/LoginPage.py
+++ Program/LoginPage.py
@@ -1,6 +1,8 @@
 from kivy.uix.screenmanager import Screen
 from kivy.lang import Builder
+
 import DB_Manager
+import MainPage as Main
 
 
 class LoginWidget(Screen):
@@ -11,10 +13,21 @@ class LoginWidget(Screen):
     def get_logged_in_user(self):
         return self.logged_in_user
 
+    def show_hide_pw(self):
+        if self.ids['login_password'].password:
+            self.ids['show_hide_btn'].icon = 'eye'
+            self.ids['login_password'].password = False
+        else:
+            self.ids['show_hide_btn'].icon = 'eye-off'
+            self.ids['login_password'].password = True
+
     def login(self):
         username = self.ids.login_username.text
         password = self.ids.login_password.text
-        logged_in_user = DB_Manager.login_user(username, password)
+        self.logged_in_user = DB_Manager.login_user(username, password)
 
-        if (logged_in_user.uid != None):
+        Main.getLoggedUser(self.logged_in_user)
+
+        if self.logged_in_user.uid:
             self.parent.current = 'main'
+            # MainWidget.ids.toolbar.title = self.logged_in_user.username
