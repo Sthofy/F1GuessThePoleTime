3c6e6d91d5077e190d21a270f98d55005733ae2f
Sthofy <suhajdakristof@gmail.com>
2022-09-24 17:55:07 +0200

    User Management refactor
    Add new design to User management.
    Add functionality to the user elements.

--- lib/libpy/profile.py
+++ lib/libpy/profile.py
@@ -1,9 +1,27 @@
-from kivy.factory import Factory
 from kivy.uix.screenmanager import Screen
 from kivy.uix.boxlayout import BoxLayout
-from kivymd.uix.bottomsheet import MDCustomBottomSheet
 from kivymd.theming import ThemableBehavior
+from kivymd.uix.button import MDFlatButton, MDRaisedButton
+from kivymd.uix.dialog import MDDialog
 from kivymd.uix.list import MDList
+
+import DB_Manager
+
+
+class ChangeUsername(BoxLayout):
+    pass
+
+
+class ChangeEmail(BoxLayout):
+    pass
+
+
+class ChangePhone(BoxLayout):
+    pass
+
+
+class ChangePassword(BoxLayout):
+    pass
 
 
 class ContentNavigationDrawer(BoxLayout):
@@ -15,22 +33,127 @@ class DrawerList(ThemableBehavior, MDList):
 
 
 class Profile(Screen):
-    def show_custom_bottom_sheet(self, sheet):
-        bsheet = Factory.PhoneSheet()
+    dialog = None
 
-        bsheet.ids.btn.on_release = self.printName()
+    def show_username(self):
+        if not self.dialog:
+            content_cls = ChangeUsername()
+            self.dialog = MDDialog(
+                title="Are you want to change your Username?",
+                type="custom",
+                content_cls=content_cls,
+                buttons=[
+                    MDFlatButton(
+                        text="CANCEL",
+                        on_release=self.submit
+                    ),
+                    MDRaisedButton(
+                        text="ACCEPT",
+                        on_release=lambda x: self.change(x, content_cls)
+                    ),
+                ],
+            )
+        self.dialog.open()
 
-        print(bsheet.ids.btn.on_release)
-        if sheet == 'Username':
-            self.custom_sheet = MDCustomBottomSheet(screen=Factory.UsernameSheet())
-        elif sheet == "Email":
-            self.custom_sheet = MDCustomBottomSheet(screen=Factory.EmailSheet())
-        elif sheet == "Password":
-            self.custom_sheet = MDCustomBottomSheet(screen=Factory.PasswordSheet())
-        elif sheet == "Phone":
-            self.custom_sheet = MDCustomBottomSheet(screen=bsheet)
+    def show_email(self):
+        if not self.dialog:
+            content_cls = ChangeEmail()
+            self.dialog = MDDialog(
+                title="Are you want to change your Email address?",
+                type="custom",
+                content_cls=content_cls,
+                buttons=[
+                    MDFlatButton(
+                        text="CANCEL",
+                        on_release=self.submit
+                    ),
+                    MDRaisedButton(
+                        text="ACCEPT",
+                        on_release=lambda x: self.change(x, content_cls)
+                    ),
+                ],
+            )
+        self.dialog.open()
 
-        self.custom_sheet.open()
+    def show_phone(self):
+        if not self.dialog:
+            content_cls = ChangePhone()
+            self.dialog = MDDialog(
+                title="Are you want to change your Email address?",
+                type="custom",
+                content_cls=content_cls,
+                buttons=[
+                    MDFlatButton(
+                        text="CANCEL",
+                        on_release=self.submit
+                    ),
+                    MDRaisedButton(
+                        text="ACCEPT",
+                        on_release=lambda x: self.change(x, content_cls)
+                    ),
+                ],
+            )
+        self.dialog.open()
 
-    def printName(self):
-        print("hello")
+    def show_password(self):
+        # TODO: check the old_password is equals with current user password
+        if not self.dialog:
+            content_cls = ChangePassword()
+            self.dialog = MDDialog(
+                title="Are you want to change your Password?",
+                type="custom",
+                content_cls=content_cls,
+                buttons=[
+                    MDFlatButton(
+                        text="CANCEL",
+                        on_release=self.submit
+                    ),
+                    MDRaisedButton(
+                        text="ACCEPT",
+                        on_release=lambda x: self.change(x, content_cls)
+                    ),
+                ],
+            )
+        self.dialog.open()
+
+    def show_delete(self):
+        if not self.dialog:
+            self.dialog = MDDialog(
+                title="Are you sure?",
+                text="This will delete your account.",
+                buttons=[
+                    MDFlatButton(
+                        text="CANCEL",
+                        on_release=self.submit
+                    ),
+                    MDRaisedButton(
+                        text="ACCEPT",
+                        on_release=lambda x: self.change(x, "delete")
+                    ),
+                ],
+            )
+        self.dialog.open()
+
+    def submit(self, *args):
+        self.dialog.dismiss(force=True)
+        self.dialog = None
+
+    def change(self, instance_btn, content):
+        # TODO: get the logged in user id
+        # TODO: check the input fields is not null
+        if type(content) is ChangeUsername:
+            print("U")
+            DB_Manager.update_user("username", content.ids.n_username.text, 2)
+        elif type(content) is ChangeEmail:
+            print("E")
+            DB_Manager.update_user("email", content.ids.n_email.text, 2)
+        elif type(content) is ChangePhone:
+            print("P")
+            DB_Manager.update_user("phone", content.ids.n_phone.text, 2)
+        elif type(content) is ChangePassword:
+            print("PW")
+            DB_Manager.update_user("password", content.ids.new_password.text, 2)
+        else:
+            print("D")
+            DB_Manager.update_user("delete", content, 2)
+        self.submit()
